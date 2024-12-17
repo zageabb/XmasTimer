ed67ccfbcb8eee62705fb33f4ddeb080818990c1
zageabb <zageabb@users.noreply.github.com>
2024-12-17 14:31:20 +0000

    Update XmasTimer.py

--- XmasTimer.py
+++ XmasTimer.py
@@ -5,6 +5,17 @@ from datetime import datetime, timedelta
 # Streamlit app setup
 #st.title("Countdown to Christmas")
 
+# Add a dancing Santa GIF as background or decorative element
+st.markdown(
+    """
+    <div style="text-align: center; margin-bottom: 20px;">
+        <iframe src="https://giphy.com/embed/EcWmyeOIlYQ8FloLBO/video" width="480" height="271" style="border:none;" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <p><a href="https://giphy.com/clips/santa-jingle-bells-christmas-discount-EcWmyeOIlYQ8FloLBO">via GIPHY</a></p>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
 # Define Christmas date for the current year
 def get_christmas_date():
     today = datetime.now()
@@ -35,7 +46,7 @@ while True:
 
     # Update the display
     placeholder.markdown(
-        f"<h1 style='text-align: center; font-size: 64px;'>Time to Christmas:<br> {countdown_display}</h1>",
+        f"<h1 style='text-align: center; font-size: 64px;'>Time to Christmas: {countdown_display}</h1>",
         unsafe_allow_html=True,
     )
 
